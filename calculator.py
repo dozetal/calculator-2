b533e5de3d2e1a262628808e0675250ffe71a146
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-05 17:40:04 -0700

    Working Copy Calculator-2

--- calculator.py
+++ calculator.py
@@ -7,62 +7,55 @@ calculator program yourself in this file.
 from arithmetic import *
 
 
-# Your code goes here
-# No setup
-# repeat forever:
-#     read input
-#     tokenize input
-#     if the first token is "q":
-#         quit
-#     else:
-#         decide which math function to call based on first token
+def evaluate_input(operand, parameter_1, parameter_2 = 0):
 
-# do_setup()
-# while exit_condition_not_reached:
-#     input = consume_input()
-#     output = evaluate_input(input)
-#     print output
+    """ 
+    Step 1 of REPL
+    This takes in the parameters and does the operand
+    """
+
+    if operand == "+":
+        return add(parameter_1, parameter_2)
+
+    elif operand == "-":
+        return subtract(parameter_1, parameter_2)
+
+    elif operand == "*":
+        return multiply(parameter_1, parameter_2)
+
+    elif operand == "/":
+        return divide(parameter_1, parameter_2)
+
+    elif operand == "square":
+        return square(parameter_1)
+
+    elif operand == "cube":
+        return cube(parameter_1)
+
+    elif operand == "pow":
+        return power(parameter_1, parameter_2)
+
+    elif operand == "mod":
+        return mod(parameter_1, parameter_2)
 
 # The While Loop runs the Calculator until told to Exit
 
 while True:
     calculator_input = raw_input(">>>")
+    
+    # Here we tokenize the input 
+    
     calculator_token = calculator_input.split(" ")
 
-# Here we tokenize the input 
+    # Check for exit variable
 
     if calculator_token[0] == "q":
-        quit
+        quit()
 
-    elif:
-        output = evaluate_input()
+    else:
+        if len(calculator_token) == 3:
+            output = evaluate_input(calculator_token[0], int(calculator_token[1]), int(calculator_token[2]))
+        else:
+            output = evaluate_input(calculator_token[0], int(calculator_token[1]))
+        
         print output
-
-
-def evaluate_input(operand, parameter_1, parameter_2 = " "):
-
-        if operand == "+":
-            return add(parameter_1, parameter_2)
-
-        elif operand == "-":
-            return subtract(parameter_1, parameter_2)
-
-        elif operand == "*":
-            return multiply(parameter_1, parameter_2)
-
-        elif operand == "/":
-            return divide(parameter_1, parameter_2)
-
-        elif operand == "square":
-            return square(parameter_1)
-
-        elif operand == "cube":
-            return cube(parameter_1)
-
-        elif operand == "pow":
-            return power(parameter_1, parameter_2)
-
-        elif operand == "mod":
-            return mod(parameter_1, parameter_2)
-
-    
