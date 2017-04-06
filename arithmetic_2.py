0a29a099cc57c447d3a08c9df3a7be242d0991af
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-06 12:48:26 -0700

    Revisions with List to Calculator 2

---
+++ arithmetic_2.py
@@ -0,0 +1,54 @@
+# This is part 2 of calculator-2 in which all functions take lists as inputs
+
+def add(num_list):
+    """Return the sum of two numbers incl list"""
+    sum = 0
+    for item in num_list:
+        sum = sum + item
+    return sum
+
+# def add(num1, num2):
+#     """Return the sum of two numbers"""
+#     return num1 + num2
+
+def subtract(num_list):
+    """Return the difference of two numbers"""
+    answer = 0
+    for item in num_list:
+        answer = answer - item
+    return answer    
+
+def multiply(num1, num2):
+    """Return the product of two numbers"""
+    return num1 * num2
+
+def divide(num1, num2):
+    """Return the quotient of two numbers as a float"""
+    return float(num1) / float(num2)
+
+def square(num):
+    """Return the square of a number"""
+    return num **2
+
+def cube(num):
+    """Return the cube of a number"""
+    return num **3
+
+def power(num, exponent):
+    """Return num raised to the power of exponent"""
+    return num **exponent
+
+def mod(num1, num2):
+    """Return remainder of num1 divided by num2"""
+    return num1 % num2
+
+def add_mult(num1, num2, num3):
+    """
+    This takes three numbers
+    It adds the first two numbers and returns the sum
+    Then it muliples the sum by the third number
+    and returns that result 
+    """
+    
+    sum1 = add(num1, num2)
+    return sum1 * num3
---
+++ calculator_2.py
@@ -0,0 +1,69 @@
+"""A prefix-notation calculator.
+
+Using the arithmetic-2.py file, adding list to the function variables in 
+Calculator Part 1. 
+"""
+
+from arithmetic_2 import *
+
+
+def evaluate_input(operand, parameter_1, parameter_2 = 0):
+
+    """ 
+    Step 1 of REPL
+    This takes in the parameters and does the operand
+    """
+
+    if operand == "+":
+        return add(parameter_1, parameter_2)
+
+    elif operand == "-":
+        return subtract(parameter_1, parameter_2)
+
+    elif operand == "*":
+        return multiply(parameter_1, parameter_2)
+
+    elif operand == "/":
+        return divide(parameter_1, parameter_2)
+
+    elif operand == "square":
+        return square(parameter_1)
+
+    elif operand == "cube":
+        return cube(parameter_1)
+
+    elif operand == "pow":
+        return power(parameter_1, parameter_2)
+
+    elif operand == "mod":
+        return mod(parameter_1, parameter_2)
+
+# The While Loop runs the Calculator until told to Exit
+
+while True:
+    calculator_input = raw_input(">>>")
+    
+    # Here we tokenize the input 
+    
+    calculator_token = calculator_input.split(" ")
+
+# Here we define the operand as the first item in the list calculator_token
+# With the numbers as the contents of the list, starting at second item in 
+# list calculator_toke
+
+    operand = calculator_token[0]
+    nums = calculator_token[1:]
+
+    # Check for exit variable
+
+    if operand == "q":
+        quit()
+
+    else:
+        output = evaluate_input(operand, nums)
+        # if len(calculator_token) == 3:
+        #     output = evaluate_input(calculator_token[0], int(calculator_token[1]), int(calculator_token[2]))
+        # else:
+        #     output = evaluate_input(calculator_token[0], int(calculator_token[1]))
+        
+        print output
